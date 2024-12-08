1fbfd26dacc30f4122c63454e4121c912db23051
leanexe <juravschiruslan@gmail.com>
2024-12-08 13:59:24 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "212625740029"
+global.ownername = "𝚘𝚖𝚒𝚗𝚘𝚞𝚜 𝚐𝚘𝚍"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
-global.location = "India, Up, Rampur"
+global.location = "𝙸𝚝𝚊𝚕𝚢 𝚋𝚛𝚎𝚜𝚌𝚒𝚊"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
+global.ownernumber = '212625740029'  //creator number
+global.ownername = '𝚘𝚖𝚒𝚗𝚘𝚞𝚜 𝚐𝚘𝚍' //𝚘𝚠𝚗𝚎𝚛𝚜𝚑𝚒𝚙 name
 global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
@@ -38,7 +38,7 @@ global.prefa = ['','!','.','#','&']
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = true //auto typing + recording
-global.autoread = false //auto read messages
+global.autoread = false  //auto read messages
 global.autobio = true //auto update bio
 global.anti92 = false //auto block +92 
 global.autoswview = true //auto view status/story
